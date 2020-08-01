78c5f471b7e647f190f2d1bb74da2878b890ab73
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-08-01 14:04:55 -0300

    fix: Páginas iniciais criadas

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashborad from '../pages/Dashborad';
+import Profile from '../pages/Profile';
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createStackNavigator();
 
@@ -13,6 +16,10 @@ const AppRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashborad" component={Dashborad} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
